e9f7ebd69b1189304f95e8f00ebd5e0ce7249dcf
Kelocoes <kevin.rodriguez109@gmail.com>
2025-02-04 18:46:40 -0500

    Upload first class

---
+++ class-1/main.js
@@ -0,0 +1,22 @@
+// "use strict";
+// x = 3.14; // This will cause an error because x is not declared
+// console.log(x)
+
+// Working with primitive data types
+// var numberA = 10;
+// console.log(typeof numberA); // number
+// var numberB = 3.14;
+// console.log(typeof numberA == typeof numberB); // true
+
+// var word = "Hello";
+// console.log(typeof word); // string
+
+// var flag = true;
+// console.log(typeof flag); // boolean
+
+// var imNotDefined;
+// var imNull = null;
+
+// console.log(typeof imNotDefined); // undefined
+// console.log(typeof imNull); // object
+// console.log(imNotDefined == imNull); // true
